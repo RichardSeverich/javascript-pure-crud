b6344898eb047f32fa6492780cf34d48c3dbc299
rseveric <rseveric@pros.com>
2020-08-29 12:47:19 -0700

    refactor

--- src/javascript/utils-local-storage.js
+++ src/javascript/utils-local-storage.js
@@ -1,11 +1,20 @@
-function storeData(variableName, data){
+function storeData(variableName, data) {
   let stringifyArrayUsers = JSON.stringify(data);
-  localStorage.setItem(variableName, stringifyArrayUsers); 
+  localStorage.setItem(variableName, stringifyArrayUsers);
 }
 
-function getData(variableName){
+function getData(variableName) {
   let retrievedObject = localStorage.getItem(variableName);
-  let data = JSON.parse(retrievedObject)
+  let data = JSON.parse(retrievedObject);
   return data;
 }
 
+function setUpStorage() {
+  let array = localStorage.getItem("arrayUsers");
+  if (array === null || array.length == 2) {
+    localStorage.setItem("edit", "false");
+    let arrayUsers = [];
+    storeData("arrayUsers", arrayUsers);
+    defaultData();
+  }
+}
